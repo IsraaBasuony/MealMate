e04ad7104912fdb7682506d939645095975ee93a
Isra Muhammed <isramuhammad613@gmail.com>
2024-02-11 20:13:07 +0200

    initial Commit

---
+++ app/src/main/java/com/iti/mealmate/WelcomeFragment.java
@@ -0,0 +1,25 @@
+package com.iti.mealmate;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WelcomeFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+}
