af0824db81d074280e8fff277bfa063f6cd4ff44
Isra Muhammed <isramuhammad613@gmail.com>
2024-02-11 21:15:12 +0200

    Splash&Welcome&LoginUI

--- app/src/main/java/com/iti/mealmate/SplashFragment.java
+++ app/src/main/java/com/iti/mealmate/SplashFragment.java
@@ -2,8 +2,12 @@ package com.iti.mealmate;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,5 +26,16 @@ public class SplashFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_welcomeFragment);
+            }
+        }, 5000);
     }
 }
--- app/src/main/java/com/iti/mealmate/WelcomeFragment.java
+++ app/src/main/java/com/iti/mealmate/WelcomeFragment.java
@@ -2,14 +2,20 @@ package com.iti.mealmate;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.iti.mealmate.databinding.FragmentWelcomeBinding;
+
 public class WelcomeFragment extends Fragment {
 
+    FragmentWelcomeBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,23 @@ public class WelcomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        binding = FragmentWelcomeBinding.inflate(inflater,container,false);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_signInFragment);
+            }
+        });
+
+    }
+
+
 }
